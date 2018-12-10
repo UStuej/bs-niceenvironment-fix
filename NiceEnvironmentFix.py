47d55b52714a465304ea9927b2dd50cd3344c69d
UStuej <45504535+UStuej@users.noreply.github.com>
2018-12-10 12:23:00 -0600

    Shortened time at end of program

--- NiceEnvironmentFix.py
+++ NiceEnvironmentFix.py
@@ -8,7 +8,7 @@ try:
     f = open(lvlp+"/info.json","r")
 except FileNotFoundError:
     print("FileNotFoundError:  Level does not exist!")
-    sleep(1)
+    sleep(0.5)
     quit()
 info_list = f.read().split('"')
 if info_list[info_list.index("environmentName")+2] != "NiceEnvironment":
@@ -66,4 +66,4 @@ for dif in ("Easy","Normal","Hard","Expert","ExpertPlus"):
         pass
 
 print("Done!")
-sleep(1)
+sleep(0.5)
